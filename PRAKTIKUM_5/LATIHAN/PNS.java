ce953a93bae4aa793a0eb3c80df5bf49b6806e18
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2025-03-26 00:20:23 +0700

    Update PNS.java

--- PRAKTIKUM_5/LATIHAN/PNS.java
+++ PRAKTIKUM_5/LATIHAN/PNS.java
@@ -1,42 +1,53 @@
 /* Nama File : PNS.java
- * Pembuat : Cikal Wahyuning Bachtiar / 24060123140175
- * Tanggal : Jumat, 21 Maret 2025
+ * Pembuat  : Cikal Wahyuning Bachtiar / 24060123140175
+ * Tanggal  : Jumat, 21 Maret 2025
  */
 
 package LATIHAN_PRAK5;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-
+ 
 public class PNS extends Manusia implements Pajak {
+
+    // Atribut
     private String nip;
-    private static int hitungPNS;
-    private final int A = 7;
-
-    public PNS(String nama, LocalDate tanggalMulaiKerja, String alamat, double pendapatan, String nip) {
-        super(nama, tanggalMulaiKerja, alamat, pendapatan);
+    private static int counterPNS = 0;
+    
+    // Method
+    public PNS(String nama, LocalDate tglMulaiKerja, String alamat, double pendapatan, String nip) {
+        super(nama, tglMulaiKerja, alamat, pendapatan);
         this.nip = nip;
-        hitungPNS++;
+        counterPNS++;
     }
-
-    public static int getHitung() { 
-        return hitungPNS; 
+ 
+    public String getNip() {
+        return nip;
     }
-
+ 
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+ 
+    public static int getCounterPNS() {
+        return counterPNS;
+    }
+ 
     @Override
     public int hitungMasaKerja() {
-        return (int) ChronoUnit.YEARS.between(tanggalMulaiKerja, LocalDate.now()) + A;
+        int A = 0;  
+        return (int) ChronoUnit.YEARS.between(getTanggalMulaiKerja(), LocalDate.now()) + A;
     }
-
+ 
     @Override
     public double hitungPajak() {
-        return 0.1 * pendapatan;
+        return 0.10 * pendapatan;
     }
-
+    
     // Menampilkan Informasi
     @Override
     public void cetakInfo() {
         super.cetakInfo();
-        System.out.println("NIP: " + nip);
+        System.out.println("NIP        : " + nip);
     }
-    
-}
+} 
