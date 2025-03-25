a5051d340257799b79c9993fdb933877a60b32bd
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2025-03-26 00:21:05 +0700

    Update Petani.java

--- PRAKTIKUM_5/LATIHAN/Petani.java
+++ PRAKTIKUM_5/LATIHAN/Petani.java
@@ -1,35 +1,53 @@
 /* Nama File : Petani.java
- * Pembuat : Cikal Wahyuning Bachtiar / 24060123140175
- * Tanggal : Jumat, 21 Maret 2025
+ * Pembuat  : Cikal Wahyuning Bachtiar / 24060123140175
+ * Tanggal  : Jumat, 21 Maret 2025
  */
 
-package LATIHAN_PRAK5;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+ package LATIHAN_PRAK5;
 
-public class Petani extends Manusia {
+ import java.time.LocalDate;
+ import java.time.temporal.ChronoUnit;
+ 
+ public class Petani extends Manusia implements Pajak {
+
+    // Atribut
     private String asalKota;
-    private static int hitungPetani;
-    private final int C = 0;
-
-    public Petani(String nama, LocalDate tanggalMulaiKerja, String alamat, double pendapatan, String asalKota) {
-        super(nama, tanggalMulaiKerja, alamat, pendapatan);
+    private static int counterPetani = 0;
+    
+    // Method
+    public Petani(String nama, LocalDate tglMulaiKerja, String alamat, double pendapatan, String asalKota) {
+        super(nama, tglMulaiKerja, alamat, pendapatan);
         this.asalKota = asalKota;
-        hitungPetani++;
+        counterPetani++;
     }
-
-    public static int getHitung() { 
-        return hitungPetani; 
+ 
+    public String getAsalKota() {
+        return asalKota;
     }
-
+ 
+    public void setAsalKota(String asalKota) {
+        this.asalKota = asalKota;
+    }
+ 
+    public static int getCounterPetani() {
+        return counterPetani;
+    }
+ 
     @Override
     public int hitungMasaKerja() {
-        return (int) ChronoUnit.YEARS.between(tanggalMulaiKerja, LocalDate.now()) + C;
+        int C = 1; 
+        return (int) ChronoUnit.YEARS.between(getTanggalMulaiKerja(), LocalDate.now()) + C;
     }
-
+ 
+    @Override
+    public double hitungPajak() {
+        return 0;
+    }
+    
+    // Menampilkan Informasi
     @Override
     public void cetakInfo() {
-    super.cetakInfo();
-    System.out.println("Asal Kota: " + asalKota);
-}
+        super.cetakInfo();
+        System.out.println("Asal Kota: " + asalKota);
+    }
 }
