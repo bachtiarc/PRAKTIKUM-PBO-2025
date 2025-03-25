0392b054761e6d7d96694a2ef5aa1a4e70c6ebbf
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2025-03-26 00:20:47 +0700

    Update Pengusaha.java

--- PRAKTIKUM_5/LATIHAN/Pengusaha.java
+++ PRAKTIKUM_5/LATIHAN/Pengusaha.java
@@ -1,40 +1,52 @@
 /* Nama File : Pengusaha.java
- * Pembuat : Cikal Wahyuning Bachtiar / 24060123140175
- * Tanggal : Jumat, 21 Maret 2025
+ * Pembuat  : Cikal Wahyuning Bachtiar / 24060123140175
+ * Tanggal  : Jumat, 21 Maret 2025
  */
 
 package LATIHAN_PRAK5;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-
+ 
 public class Pengusaha extends Manusia implements Pajak {
+
+    // Atribut
     private String npwp;
-    private static int hitungPengusaha;
-    private final int B = 5;
-
-    public Pengusaha(String nama, LocalDate tanggalMulaiKerja, String alamat, double pendapatan, String npwp) {
-        super(nama, tanggalMulaiKerja, alamat, pendapatan);
+    private static int counterPengusaha = 0;
+    
+    // Method
+    public Pengusaha(String nama, LocalDate tglMulaiKerja, String alamat, double pendapatan, String npwp) {
+        super(nama, tglMulaiKerja, alamat, pendapatan);
         this.npwp = npwp;
-        hitungPengusaha++;
+        counterPengusaha++;
     }
-
-    public static int getHitung() { 
-        return hitungPengusaha; 
+ 
+    public String getNpwp() {
+        return npwp;
     }
-
+ 
+    public void setNpwp(String npwp) {
+        this.npwp = npwp;
+    }
+ 
+    public static int getCounterPengusaha() {
+        return counterPengusaha;
+    }
+ 
     @Override
     public int hitungMasaKerja() {
-        return (int) ChronoUnit.YEARS.between(tanggalMulaiKerja, LocalDate.now()) + B;
+        int B = 7; 
+        return (int) ChronoUnit.YEARS.between(getTanggalMulaiKerja(), LocalDate.now()) + B;
     }
-
+ 
     @Override
     public double hitungPajak() {
         return 0.15 * pendapatan;
     }
-
+ 
     @Override
     public void cetakInfo() {
-    super.cetakInfo();
-    System.out.println("NPWP: " + npwp);
-}
-}
+        super.cetakInfo();
+        System.out.println("NPWP       : " + npwp);
+    }
+} 
