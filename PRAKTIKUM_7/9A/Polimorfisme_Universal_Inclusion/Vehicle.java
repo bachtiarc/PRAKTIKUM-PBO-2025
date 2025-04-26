5019a40c2536599f104e730befdef0cfc111630e
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2025-04-26 10:44:29 +0700

    Create Vehicle.java

---
+++ PRAKTIKUM_7/9A/Polimorfisme_Universal_Inclusion/Vehicle.java
@@ -0,0 +1,11 @@
+/* Nama File : Vehicle.java
+ * Pembuat : Cikal Wahyuning Bachtiar / 24060123140175
+ * Tanggal : Selasa, 22 April 2025
+ */
+
+public class Vehicle {
+    void calRent(int distance, float price) {
+        float fare = distance * price;
+        System.out.println("Vehicle price = " + fare);
+    }
+}
