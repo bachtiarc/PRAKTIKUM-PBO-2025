456ae2fd1eed89b5e3307e42133faf0ecd0b1463
Cikal Wahyuning Bachtiar <cikalwahyuning19@gmail.com>
2025-05-04 00:16:28 +0700

    Create Anabul.java

---
+++ PRAKTIKUM_9/soal_A_B/Anabul.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal_A_B;
+
+/**
+ *
+ * @author cikalbachtiar
+ */
+/* Nama File : Anabul.java
+ * Pembuat : Cikal Wahyuning Bachtiar / 24060123140175
+ * Tanggal : Selasa, 29 April 2025
+ */
+
+public class Anabul {
+    // Atribut
+    protected String nama;
+
+    // Method
+    public void gerak() {
+        System.out.println("Gerak umum");
+    }
+
+    public void bersuara() {
+        System.out.println("Suara umum");
+    }
+}
+
